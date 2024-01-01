171830b85cd36cc853c2b67d2159b63adf1ab0f4
sudarsanselvane <shaan31885@gmail.com>
2024-01-01 13:08:55 -0500

    Hello World base

---
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.stacksimplify.restservices.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+	
+	// Simple Method
+	// URI - /helloworld
+	// GET
+	//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+	@GetMapping("/helloworld1")
+	public String helloWorld() {
+		return "Hello World1";
+		
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("Sudar", "Selvane", "Charlotte");
+	}
+	
+	
+}
---
+++ src/main/java/com/stacksimplify/restservices/Hello/UserDetails.java
@@ -0,0 +1,47 @@
+package com.stacksimplify.restservices.Hello;
+
+public class UserDetails {
+
+	private String firstname;
+	private String lastname;
+	private String city;
+	
+	
+	//Fields constructors
+	public UserDetails(String firstname, String lastname, String city) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.city = city;
+	}
+	
+	
+	//Getters and setters
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	// To String
+	@Override
+	public String toString() {
+		return "UserDetails [firstname=" + firstname + ", lastname=" + lastname + ", city=" + city + "]";
+	}
+	
+	
+	
+}
